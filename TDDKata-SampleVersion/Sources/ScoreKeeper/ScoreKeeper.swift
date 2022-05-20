70b110ce164311a1fc41939b5d0e5cd813c780f0
Tsungyu Yu <ytyubox@gmail.com>
2022-05-20 21:49:23 +0800

    ScoreKeeper in production

--- TDDKata-SampleVersion/Sources/ScoreKeeper/ScoreKeeper.swift
+++ TDDKata-SampleVersion/Sources/ScoreKeeper/ScoreKeeper.swift
@@ -3,3 +3,38 @@
 //
 
 import Foundation
+public final class ScoreKeeper {
+    public init() {}
+    public var score:String {
+        let n1 = String(format: "%03d", min(999,(max(teamAScore, 0))))
+        let n2 = String(format: "%03d", min(999,(max(teamBScore, 0))))
+        return "\(n1)-\(n2)" 
+    }
+    var teamAScore = 0
+    var teamBScore = 0
+    public func getScore() -> String {
+        return score
+    }
+
+    public func scoreTeamA1() {
+        teamAScore += 1
+    }
+    public func scoreTeamA2() {
+        teamAScore += 2
+    }
+    public func scoreTeamA3() {
+        teamAScore += 3
+        
+    }
+    public func scoreTeamB1() {
+        teamBScore += 1
+       
+    }
+    public func scoreTeamB2() {
+        teamBScore += 2
+       
+    }
+    public func scoreTeamB3() {
+        teamBScore += 3
+    }
+}
--- TDDKata-SampleVersion/Tests/ScoreKeeperTests/ScoreKeeperTests.swift
+++ TDDKata-SampleVersion/Tests/ScoreKeeperTests/ScoreKeeperTests.swift
@@ -4,8 +4,78 @@
 
 import Foundation
 import XCTest
+import ScoreKeeper
+
 class ScoreKeeperTests: XCTestCase {
+    private let readme = """
+    ## Problem
+    We need software to deliver the proper data to the scoreboard for a basketball team. Unfortunately the people using our software are not the brightest lights under the sun, so they need six buttons (each team can score either 1, 2 or 3 points with a single shot).
+
+    ## Your Task
+    Write a class `ScoreKeeper` which offers following methods:
+    ```
+    void scoreTeamA1()
+    void scoreTeamA2()
+    void scoreTeamA3()
+    void scoreTeamB1()
+    void scoreTeamB2()
+    void scoreTeamB3()
+    String getScore()
+    ```
+    ## Rules
+    The returned String always has seven characters. An example would be 000:000
+    """
     func test() {
-        XCTFail("👏 Great! you are running this the right test! Please remove this line to start")
+        let sut = ScoreKeeper()
+        XCTAssertEqual(sut.getScore(), "000-000")
+        sut.scoreTeamA1()
+        XCTAssertEqual(sut.getScore(), "001-000")
+        sut.scoreTeamA2()
+        XCTAssertEqual(sut.getScore(), "003-000")
+         sut.scoreTeamA3()
+        XCTAssertEqual(sut.getScore(), "006-000")
+         sut.scoreTeamA1()
+        XCTAssertEqual(sut.getScore(), "007-000")
+        sut.scoreTeamA2()
+        XCTAssertEqual(sut.getScore(), "009-000")
+         sut.scoreTeamA3()
+        XCTAssertEqual(sut.getScore(), "012-000")
+
+        sut.scoreTeamB1()
+        XCTAssertEqual(sut.getScore(), "012-001")
+        sut.scoreTeamB2()
+        XCTAssertEqual(sut.getScore(), "012-003")
+         sut.scoreTeamB3()
+        XCTAssertEqual(sut.getScore(), "012-006")
+         sut.scoreTeamB1()
+        XCTAssertEqual(sut.getScore(), "012-007")
+        sut.scoreTeamB2()
+        XCTAssertEqual(sut.getScore(), "012-009")
+         sut.scoreTeamB3()
+        XCTAssertEqual(sut.getScore(), "012-012")
+    }
+
+    func test_ScoreCountShouldAlways7Digits() {
+        let sut = ScoreKeeper()
+        for _ in 1 ... 999 {
+            sut.scoreTeamA1()
+        }
+        XCTAssertEqual(sut.score, "999-000")
+        sut.scoreTeamA1()
+        XCTAssertEqual(sut.score, "999-000")
+        sut.scoreTeamA2()
+        XCTAssertEqual(sut.score, "999-000")
+        sut.scoreTeamA3()
+        XCTAssertEqual(sut.score, "999-000")
+        for _ in 1 ... 999 {
+            sut.scoreTeamB1()
+        }
+         XCTAssertEqual(sut.score, "999-999")
+        sut.scoreTeamB1()
+        XCTAssertEqual(sut.score, "999-999")
+        sut.scoreTeamB2()
+        XCTAssertEqual(sut.score, "999-999")
+        sut.scoreTeamB3()
+        XCTAssertEqual(sut.score, "999-999")
     }
 }
